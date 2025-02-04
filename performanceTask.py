902957fd615795a53e78cc16d1c3f0c0a348e72a
Mendez <mmendez33@cps.k12.il.us>
2025-02-04 09:30:16 -0600

    first commit

---
+++ performanceTask.py
@@ -0,0 +1,21 @@
+######### MUSIC RECOMMENDATION TOOL##########################
+
+ask = input("Welcome to the Music Recommendation Tool! Would you like recommendations based on genre or mood?")
+moods= ["Happy" , "Sad" ,  "Energized" , "Calm", "Nostalgic"]
+genres = ["Pop", "Emo", "Indie", "R&B", "K-pop", "Rap" ]
+
+happy_songs = [""]
+sad_songs = []
+energized_songs =[]
+calm_songs = []
+nostalgic_songs = []
+
+pop_songs = []
+emo_songs = []
+indie_songs = []
+rb_songs = []
+kpop_songs =[]
+rap_songs = []
+
+
+if ask == "mood":
